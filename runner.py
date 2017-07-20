29d2ccce550490389a4312245f40e42448f09b54
Benton <bmize@typezero.com>
2017-07-20 08:40:39 -0400

    Linting

--- runner.py
+++ runner.py
@@ -1,29 +1,29 @@
 # Imports the monkeyrunner modules used by this program
-from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 import time
 import sys
 import os
+from com.android.monkeyrunner import MonkeyRunner
 
 def main():
     print("Sir, there are still terabytes of calculations required before an actual flight is...")
 
-    # Get all device that connected with USB 
+    # Get all device that connected with USB
     connected_devices = os.popen('adb devices').read().strip().split('\n')[1:]
     devices = []
-    for deviceId in connected_devices:
+    for device_id in connected_devices:
         # Get device name
-        deviceName = deviceId.split('\t')[0]
-        print('deviceName: ' + deviceName)
-        
-        # Get connection of device 
-        device = MonkeyRunner.waitForConnection(10.0, deviceName)
+        serial = device_id.split('\t')[0]
+        print('Device Serial: ' + serial)
+
+        # Get connection of device
+        device = MonkeyRunner.waitForConnection(10.0, serial)
         devices.append(device)
 
     # Set start activity
     for device in devices:
         device.startActivity(component='com.android.settings/.Settings')
-    
-    print ('startActivity: Settings')
+
+    print('startActivity: Settings')
 
 if __name__ == "__main__":
     main()
